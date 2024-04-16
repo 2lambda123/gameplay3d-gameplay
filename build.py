ccd25a5913d625866d2c2ccd46e9ab037c572f29
Restyled.io <commits@restyled.io>
2024-04-16 12:08:54 +0000

    Restyled by black

--- build.py
+++ build.py
@@ -15,7 +15,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--clean", help="Clean only", action="store_true")
 parser.add_argument("-g", "--generate", help="Generates only", action="store_true")
 parser.add_argument("-b", "--build", help="Builds only", action="store_true")
-parser.add_argument("--configuration", help="Specify the configuration. (all,debug,release)", default="all")
+parser.add_argument(
+    "--configuration",
+    help="Specify the configuration. (all,debug,release)",
+    default="all",
+)
 parser.add_argument("--toolchain", help="Toolchain override.", default="")
 args = parser.parse_args()
 
@@ -29,6 +33,7 @@ DEPS_FOLDER = "_deps"
 # utility functions
 ##############################################################################
 
+
 def clear_dir(dir_path):
     if os.path.exists(dir_path):
         shutil.rmtree(dir_path, ignore_errors=True)
@@ -37,27 +42,35 @@ def clear_dir(dir_path):
     if os.path.exists(dir_path):
         os.rmdir(dir_path)
 
+
 def copy_files(src_dir, dst_dir, match_exp):
     clear_dir(dst_dir)
     for filename in glob.glob(os.path.join(src_dir, match_exp)):
         shutil.copy(filename, dst_dir)
 
+
 def remove_files(src_dir, match_exp):
     for filename in glob.glob(os.path.join(src_dir, match_exp)):
         os.remove(filename)
+
 
 def init_vsvars():
     vswhere_path = r"%ProgramFiles(x86)%/Microsoft Visual Studio/Installer/vswhere.exe"
     vswhere_path = os.path.expandvars(vswhere_path)
     if not os.path.exists(vswhere_path):
         raise EnvironmentError("vswhere.exe not found at: %s", vswhere_path)
-    vs_path = os.popen('"{}" -latest -property installationPath'.format(vswhere_path)).read().rstrip()
+    vs_path = (
+        os.popen('"{}" -latest -property installationPath'.format(vswhere_path))
+        .read()
+        .rstrip()
+    )
     vsvars_path = os.path.join(vs_path, "VC\\Auxiliary\\Build\\vcvars64.bat")
     output = os.popen('"{}" && set'.format(vsvars_path)).read()
     for line in output.splitlines():
         pair = line.split("=", 1)
-        if (len(pair) >= 2):
+        if len(pair) >= 2:
             os.environ[pair[0]] = pair[1]
+
 
 # building
 ##############################################################################
@@ -79,7 +92,11 @@ else:
 compiler_dir = os.path.join(current_dir, COMPILER_FOLDER)
 if not args.build and not args.clean:
     deps_dir = os.path.join(current_dir, DEPS_FOLDER)
-    premake_proc = subprocess.Popen(f"{deps_dir}/premake/premake5 --file=premake5.lua {toolchain}", cwd=current_dir, shell=True)
+    premake_proc = subprocess.Popen(
+        f"{deps_dir}/premake/premake5 --file=premake5.lua {toolchain}",
+        cwd=current_dir,
+        shell=True,
+    )
     premake_proc.wait()
 
 # clean build check
@@ -130,12 +147,20 @@ if not args.generate:
         compiler_dir = os.path.join(compiler_dir, "xcode4")
         os.chdir(compiler_dir)
         if clean_build:
-            subprocess.run("xcodebuild clean -workspace gameplay.xcworkspace", shell=True)
+            subprocess.run(
+                "xcodebuild clean -workspace gameplay.xcworkspace", shell=True
+            )
             clear_dir(build_dir)
         else:
             if config_debug:
-                subprocess.run("xcodebuild -workspace gameplay.xcworkspace -configuration Debug build", shell=True)
+                subprocess.run(
+                    "xcodebuild -workspace gameplay.xcworkspace -configuration Debug build",
+                    shell=True,
+                )
             if config_release:
-                subprocess.run("xcodebuild -workspace gameplay.xcworkspace -configuration Release build", shell=True)
+                subprocess.run(
+                    "xcodebuild -workspace gameplay.xcworkspace -configuration Release build",
+                    shell=True,
+                )
     else:
         print("Error: Compiler toolchain not supported.")
